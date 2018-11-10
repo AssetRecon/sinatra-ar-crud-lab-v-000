5a9fbf7c787d555cac5528ebc04cfeb2839d0c51
Edsel Caprice <caprice@mail.usf.edu>
2018-11-10 12:44:03 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,10 +3,15 @@
 class Post < ActiveRecord::Base
   attr_accessor :name, :content
 
+  @@posts = []
+
   def self.create(name, content)
     @name = name
     @content = content
   end
 
+  def self.all
+    @@all
+  end
   
 end
