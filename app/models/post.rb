72a84052c09fe43fde206e48a5dacff1b5d21a7a
Edsel Caprice <caprice@mail.usf.edu>
2018-11-10 12:37:04 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@
 class Post < ActiveRecord::Base
   attr_accessor :name, :content
 
-  def initialize(name, content)
+  def initialize(name, content = nil)
     @name = name
     @content = content
   end
